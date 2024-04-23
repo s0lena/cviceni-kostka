d1cfc57c959b1017f01bb194595a7749a61f1944
s0lena <solena.work@gmail.com>
2024-04-23 14:45:23 +0300

    Added the dice side change on keydown event

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const kostkaSide = () => {
+    let kostkaNumber = Math.floor(Math.random()*6 +1);
+    document.querySelector('img').src=`img/side${kostkaNumber}.svg`
+}
 
+document.body.addEventListener('keydown', kostkaSide);
